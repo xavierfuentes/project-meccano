8bdee4fc7bb2bbfd31ff4b938f4b85c67bfefbfa
Xavi Fuentes <xavifuefer@gmail.com>
2017-03-22 21:55:01 +0000

    improve code quality

--- src/modules/UserManager/data/Signup/reducer.js
+++ src/modules/UserManager/data/Signup/reducer.js
@@ -11,37 +11,36 @@ const initialState = fromJS({
 
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case types.SIGNUP_REQUESTING:
-      return state.set('requesting', true).set(
-        'messages',
-        fromJS([
-          {
-            body: 'Signing up...',
-            time: new Date(),
-          },
-        ])
-      );
+    case types.SIGNUP_REQUESTING: {
+      const message = fromJS({
+        body: 'Signing up...',
+        time: new Date(),
+      });
+      return state.set('requesting', true).set('messages', state.get('messages').push(message));
+    }
 
-    case types.SIGNUP_SUCCESS:
-      return state.set('requesting', false).set('successful', true).set(
-        'messages',
-        fromJS({
-          body: `Successfully created account for ${payload.response.user.email}`,
-          time: new Date(),
-        })
-      );
+    case types.SIGNUP_SUCCESS: {
+      const message = fromJS({
+        body: `Successfully created account for ${payload.response.user.email}`,
+        time: new Date(),
+      });
+      return state
+        .set('requesting', false)
+        .set('successful', true)
+        .set('messages', state.get('messages').push(message));
+    }
 
-    case types.SIGNUP_ERROR:
-      return state.set('requesting', false).set('successful', false).set(
-        'errors',
-        state.get('errors').push({
-          body: payload.error.toString(),
-          time: new Date(),
-        })
-      );
+    case types.SIGNUP_ERROR: {
+      const error = fromJS({
+        body: `Successfully created account for ${payload.response.user.email}`,
+        time: new Date(),
+      });
+      return state.set('requesting', false).set('successful', false).set('errors', state.get('errors').push(error));
+    }
 
-    default:
+    default: {
       return state;
+    }
   }
 };
 
